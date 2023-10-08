4a73bc1200ac6e7bdff13c5af52e089895fd8bf9
Jhon Waree Marcelo <jhon.waree.marcelo@gmail.com>
2023-10-08 16:58:38 -0600

    pull exercise logs of current user in exercise tab

--- client/src/components/Exercise.js
+++ client/src/components/Exercise.js
@@ -1,7 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Card from "react-bootstrap/Card";
 
-function Exercise() {
-  return <div>Exercise</div>;
+import Button from "react-bootstrap/Button";
+
+function Exercise({ id }) {
+  const [exercise, setExercise] = useState([]);
+
+  useEffect(() => {
+    fetch(`/exercise/${id}`)
+      .then((r) => r.json())
+      .then((items) => setExercise(items));
+  }, []);
+
+  return (
+    <div>
+      <ul>
+        {exercise.map((exercise) => {
+          return (
+            <>
+              <Card key={exercise.id}>
+                <Card.Text>
+                  <h1>{exercise.exercise_name}</h1>
+                  <h2>sets: {exercise.sets}</h2>
+                  <h2>reps: {exercise.reps}</h2>
+                  <h2>weight: {exercise.weight}</h2>
+                </Card.Text>
+              </Card>
+            </>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
 export default Exercise;

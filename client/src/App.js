670ee15b622717acda43f2093a3829430e3952af
Jhon Waree Marcelo <jhon.waree.marcelo@gmail.com>
2023-10-07 22:08:37 -0600

    added bcrypt

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.css"; //NEED THIS TO USE BOOTSRAP CSS
-import Navbar from "./components/Navbar";
+import Authentication from "./components/Authentication";
 
 function App() {
   const [data, setData] = useState({});
@@ -17,7 +17,7 @@ function App() {
 
   return (
     <div className="App">
-      <Navbar />
+      <Authentication />
     </div>
   );
 }
--- server/config.py
+++ server/config.py
@@ -7,6 +7,7 @@ from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from flask_bcrypt import Bcrypt
 
 # Local imports
 
@@ -15,6 +16,8 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
+
+app.secret_key = b'\xc5\xd0\xcbS\x82,\x07\xbf\xf4|\xad\xc4\x15\x84\xf0F'
 
 # Define metadata, instantiate db
 metadata = MetaData(naming_convention={
@@ -30,6 +33,10 @@ migrate = Migrate(app, db)
 
 # Instantiate REST API
 api = Api(app)
+
+#Instantiate Bcrypt
+bcrypt = Bcrypt(app)
+
 
 # Instantiate CORS
 CORS(app)

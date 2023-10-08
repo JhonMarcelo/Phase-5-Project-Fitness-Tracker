7a58b47053f2dc0f103fe5d8f3406b9594bd3a17
Jhon Waree Marcelo <jhon.waree.marcelo@gmail.com>
2023-10-08 06:34:50 -0600

    finished LoginForm

--- client/src/components/Forms/Login.js
+++ client/src/components/Forms/Login.js
@@ -1,7 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
-function Login() {
-  return <div>Login</div>;
+function Login({ onLogin }) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [errors, setErrors] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    }).then((r) => {
+      if (r.ok) {
+        r.json().then((user) => onLogin(user));
+      } else {
+        r.json().then((err) => setErrors(err.errors));
+      }
+    });
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <Form.Label htmlFor="inputPassword5">Username</Form.Label>
+      <Form.Control
+        type="text"
+        id="inputPassword5"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        aria-describedby="passwordHelpBlock"
+      />
+      <Form.Label htmlFor="inputPassword5">Password</Form.Label>
+      <Form.Control
+        type="password"
+        id="inputPassword5"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        aria-describedby="passwordHelpBlock"
+      />
+      <Form.Text id="passwordHelpBlock" muted>
+        Your password must be 8-20 characters long, contain letters and numbers,
+        and must not contain spaces, special characters, or emoji.
+      </Form.Text>
+      <br></br>
+      <Button variant="primary">Log in</Button>{" "}
+    </form>
+  );
 }
 
 export default Login;
--- client/src/components/Forms/Signup.js
+++ client/src/components/Forms/Signup.js
@@ -1,7 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 function Signup() {
-  return <div>Signup</div>;
-}
+  const [username, setUsername] = useState("");
+  const [first_name, setFirst_name] = useState("");
+  const [last_name, setLast_name] = useState("");
+  const [password, setPassword] = useState("");
 
+  /*FETCH HERE*/
+  return (
+    /* add onSubmit */
+    <form>
+      <Form.Label htmlFor="inputPassword5">Username</Form.Label>
+      <Form.Control
+        type="text"
+        id="inputPassword5"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        aria-describedby="passwordHelpBlock"
+      />
+      <Form.Label htmlFor="inputPassword5">First Namel</Form.Label>
+      <Form.Control
+        type="text"
+        id="inputPassword5"
+        value={username}
+        onChange={(e) => setFirst_name(e.target.value)}
+        aria-describedby="passwordHelpBlock"
+      />
+      <Form.Label htmlFor="inputPassword5">Last Name</Form.Label>
+      <Form.Control
+        type="text"
+        id="inputPassword5"
+        value={username}
+        onChange={(e) => setLast_name(e.target.value)}
+        aria-describedby="passwordHelpBlock"
+      />
+      <Form.Label htmlFor="inputPassword5">Password</Form.Label>
+      <Form.Control
+        type="password"
+        id="inputPassword5"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        aria-describedby="passwordHelpBlock"
+      />
+      <Form.Text id="passwordHelpBlock" muted>
+        Your password must be 8-20 characters long, contain letters and numbers,
+        and must not contain spaces, special characters, or emoji.
+      </Form.Text>
+      <br></br>
+      <Button variant="primary">Sign up</Button>{" "}
+    </form>
+  );
+}
 export default Signup;
